f38dd42e4dbd9a2fbe58e87916962576764e01ed
juln <zjl5@meitu.com>
2023-11-02 00:51:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# ai
+
+## run
+
+1. terminal run `poetry install`
+2. set your `OPENAI_API_KEY` by `.env` file
+3. terminal run `poetry run python src/ai-model.py`
---
+++ src/ai-model/__main__.py
@@ -0,0 +1,25 @@
+import os
+import openai
+from dotenv_flow import dotenv_flow
+
+dotenv_flow()
+
+openai.api_base = os.getenv('OPENAI_API_BASE')
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+response = openai.ChatCompletion.create(
+  model = 'gpt-3.5-turbo',
+  messages = [
+    {
+      'role': 'system',
+      'content': '你是周树人, 你爸是鲁迅, 但是你从小和父亲失散, 你不认识你爸',
+    },
+    {
+      'role': 'user',
+      'content': '鲁迅认识周树人吗?',
+    },
+  ],
+)
+
+# print(response)
+print(response.choices[0].message.content)
